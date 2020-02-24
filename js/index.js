ae24058164f51766645c9bf42fca9e46304f20b4
Gagan <gaganjot.singh.randhawa@sap.com>
2020-02-24 15:45:59 +0530

    Version 1.0 with typewriter effect and hashtext

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+/* Handle navigator and hamburger */
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link')
 
@@ -11,6 +12,7 @@ navLinks.forEach(link => {
   })
 })
 
+/* Hash text on header */
 let doc = document,
   id = (e) => {
     return doc.getElementById(e)
@@ -22,104 +24,155 @@ let doc = document,
     return doc.getElementsByClassName(e).length
   };
 
-const reveal = () => {
-  // classGet('intro', 0).innerHTML = 'Gagan Randhawa'; //change this
-  // $('#after').show();
-  // $('.icons-social').animateCss('slideInUp')
-  // $('.tagline').animateCss('slideInDown');
-  // $('#after').animateCss('slideInUp')
+
+let Messenger = function (el) {
+  let m = this;
+  m.init = function () {
+    m.codeletters = "&#*+%?£@§$";
+    m.message = 0;
+    m.current_length = 0;
+    m.fadeBuffer = false;
+    m.messages = [
+      `${superGet.innerText}`,
+    ];
+    setTimeout(m.animateIn, 100);
+  };
+  m.generateRandomString = function (length) {
+    let random_text = '';
+    while (random_text.length < length) {
+      random_text += m.codeletters.charAt(Math.floor(Math.random() * m.codeletters.length));
+    }
+    return random_text;
+  };
+  m.animateIn = function () {
+    if (m.current_length < m.messages[m.message].length) {
+      m.current_length = m.current_length + 2;
+      if (m.current_length > m.messages[m.message].length) {
+        m.current_length = m.messages[m.message].length;
+      }
+
+      let message = m.generateRandomString(m.current_length);
+      el.innerHTML = message;
+      setTimeout(m.animateIn, 20);
+    } else {
+      setTimeout(m.animateFadeBuffer, 20);
+    }
+  };
+
+  m.animateFadeBuffer = function () {
+    if (m.fadeBuffer === false) {
+      m.fadeBuffer = [];
+      for (let i = 0; i < m.messages[m.message].length; i++) {
+        m.fadeBuffer.push({
+          c: (Math.floor(Math.random() * 12)) + 1,
+          l: m.messages[m.message].charAt(i)
+        });
+      }
+    }
+
+    let do_cycles = false;
+    let message = '';
+
+    for (let i = 0; i < m.fadeBuffer.length; i++) {
+      let fader = m.fadeBuffer[i];
+      if (fader.c > 0) {
+        do_cycles = true;
+        fader.c--;
+        message += m.codeletters.charAt(Math.floor(Math.random() * m.codeletters.length));
+      } else {
+        message += fader.l;
+      }
+    }
+    el.innerHTML = message;
+
+    if (do_cycles === true) {
+      setTimeout(m.animateFadeBuffer, 50);
+    } else {
+      setTimeout(m.cycleText, 2000);
+    }
+  };
+
+  m.cycleText = function () {
+    m.message = m.message + 1;
+    if (m.message >= m.messages.length) {
+      m.message = 0;
+    }
+
+    m.current_length = 0;
+    m.fadeBuffer = false;
+    el.innerHTML = ' ';
+
+    setTimeout(m.animateIn, 200);
+  };
+
+  m.init();
 }
-let typed = new Typed('#typed', {
-  stringsElement: '#typed-strings',
-  typeSpeed: 50,
-  onComplete: reveal,
-});
 
-// let Messenger = function (el) {
-//   let m = this;
-//   m.init = function () {
-//     m.codeletters = "&#*+%?£@§$";
-//     m.message = 0;
-//     m.current_length = 0;
-//     m.fadeBuffer = false;
-//     m.messages = [
-//       `${superGet.innerText}`,
-//     ];
-//     setTimeout(m.animateIn, 100);
-//   };
-//   m.generateRandomString = function (length) {
-//     let random_text = '';
-//     while (random_text.length < length) {
-//       random_text += m.codeletters.charAt(Math.floor(Math.random() * m.codeletters.length));
-//     }
-//     return random_text;
-//   };
-//   m.animateIn = function () {
-//     if (m.current_length < m.messages[m.message].length) {
-//       m.current_length = m.current_length + 2;
-//       if (m.current_length > m.messages[m.message].length) {
-//         m.current_length = m.messages[m.message].length;
-//       }
+for (let i = 0; i < doc.getElementsByClassName('hashText').length; i++) {
+  let currentEl = doc.getElementsByClassName('hashText')[i];
+  superGet = currentEl;
+  let messenger = new Messenger(currentEl);
+}
 
-//       let message = m.generateRandomString(m.current_length);
-//       el.innerHTML = message;
-//       setTimeout(m.animateIn, 20);
-//     } else {
-//       setTimeout(m.animateFadeBuffer, 20);
-//     }
-//   };
+/* Typewriter effect */
+var TxtRotate = function (el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
 
-//   m.animateFadeBuffer = function () {
-//     if (m.fadeBuffer === false) {
-//       m.fadeBuffer = [];
-//       for (let i = 0; i < m.messages[m.message].length; i++) {
-//         m.fadeBuffer.push({
-//           c: (Math.floor(Math.random() * 12)) + 1,
-//           l: m.messages[m.message].charAt(i)
-//         });
-//       }
-//     }
+TxtRotate.prototype.tick = function () {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
 
-//     let do_cycles = false;
-//     let message = '';
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
 
-//     for (let i = 0; i < m.fadeBuffer.length; i++) {
-//       let fader = m.fadeBuffer[i];
-//       if (fader.c > 0) {
-//         do_cycles = true;
-//         fader.c--;
-//         message += m.codeletters.charAt(Math.floor(Math.random() * m.codeletters.length));
-//       } else {
-//         message += fader.l;
-//       }
-//     }
-//     el.innerHTML = message;
+  this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
 
-//     if (do_cycles === true) {
-//       setTimeout(m.animateFadeBuffer, 50);
-//     } else {
-//       setTimeout(m.cycleText, 2000);
-//     }
-//   };
+  var that = this;
+  var delta = 150 - Math.random() * 100;
 
-//   m.cycleText = function () {
-//     m.message = m.message + 1;
-//     if (m.message >= m.messages.length) {
-//       m.message = 0;
-//     }
+  if (this.isDeleting) { delta /= 2; }
 
-//     m.current_length = 0;
-//     m.fadeBuffer = false;
-//     el.innerHTML = ' ';
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+  const timer = setTimeout(function () {
+    that.tick();
+  }, delta);
+  if (this.loopNum === 5) {
+    clearTimeout(timer);
+    classGet('section__title--intro', 0).innerText = 'Gagan Randhawa';
+    //document.body.classList.toggle('nav-open'); add something here
+    window.location.hash = 'skills';
+  }
+};
 
-//     setTimeout(m.animateIn, 200);
-//   };
-
-//   m.init();
-// }
-
-// for (let i = 0; i < doc.getElementsByClassName('hashText').length; i++) {
-//   let currentEl = doc.getElementsByClassName('hashText')[i];
-//   superGet = currentEl;
-//   let messenger = new Messenger(currentEl);
-// }
+window.onload = function () {
+  var elements = document.getElementsByClassName('txt-rotate');
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-rotate');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtRotate(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #16e0bd }";
+  document.body.appendChild(css);
+};
